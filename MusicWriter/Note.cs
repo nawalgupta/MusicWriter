c8373d0fff48bee0b4ce35a58da6e609d7bf048d
Isaac V <isaacv123456789@gmail.com>
2016-10-27 08:55:33 -0500

    core code

---
+++ MusicWriter/Note.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MusicWriter {
+    public sealed class Note {
+        public Time Offset { get; set; }
+        public Time Length { get; set; }
+        public float Velocity { get; set; } = 0.5f;
+    }
+}
---
+++ MusicWriter/Time.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicWriter {
+    public struct Time {
+        int ticks;
+        
+        public double Notes {
+            get { return (double)ticks / TicksPerNote; }
+        }
+
+        public Time Double {
+            get { return new Time(ticks * 2); }
+        }
+
+        public Time Triple {
+            get { return new Time(ticks * 3); }
+        }
+
+        public Time Fifth {
+            get { return new Time(ticks * 5); }
+        }
+
+        public Time Seventh {
+            get { return new Time(ticks * 7); }
+        }
+
+        public Time(int ticks = 0) {
+            this.ticks = ticks;
+        }
+
+        public const int TicksPerNote_2nd = TicksPerNote / 2;
+        public const int TicksPerNote_4th = TicksPerNote / 4;
+        public const int TicksPerNote_8th = TicksPerNote / 8;
+        public const int TicksPerNote_16th = TicksPerNote / 16;
+        public const int TicksPerNote_32nd = TicksPerNote / 32;
+        public const int TicksPerNote_64th = TicksPerNote / 64;
+        public const int TicksPerNote_128th = TicksPerNote / 128;
+        public const int TicksPerNote_3rd = TicksPerNote / 3;
+        public const int TicksPerNote_5th = TicksPerNote / 5;
+        public const int TicksPerNote_7th = TicksPerNote / 7;
+
+        public const int TicksPerNote = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 3 * 5 * 7; // 128th notes, 3rd notes, 5th notes, 7th notes
+    }
+}
