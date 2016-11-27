876b92b35e5d403440c2220f2c782ae3e317bb89
Isaac V <isaacv123456789@gmail.com>
2016-11-27 15:24:57 -0600

    theme class definition

---
+++ MusicWriter.WinForms/InternalExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicWriter.WinForms
+{
+    public static class InternalExtensions
+    {
+        public static Color Alpha(this Color @base, double alpha) =>
+            Color.FromArgb((int)(255 * alpha), @base);
+    }
+}
---
+++ MusicWriter.WinForms/Theme.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicWriter.WinForms
+{
+    public sealed class Theme
+    {
+        public struct SheetMusicTheme
+        {
+            public struct CursorTheme
+            {
+                public Color CaretColor;
+                public float CaretWidth;
+
+                public Color ExtensionLineColor;
+                public float ExtensionLineWidth;
+            }
+
+            public struct TrackTheme
+            {
+                public Color Background;
+                public Color Foreground;
+            }
+
+            public CursorTheme CursorActive;
+            public CursorTheme CursorPassive;
+            public TrackTheme TrackActive;
+            public TrackTheme TrackPassive;
+        }
+
+        public SheetMusicTheme SheetMusicActive;
+        public SheetMusicTheme SheetMusicPassive;
+
+        public static Theme Default = new Theme {
+            SheetMusicActive = new SheetMusicTheme {
+                CursorActive = new SheetMusicTheme.CursorTheme {
+                    CaretColor = Color.LawnGreen.Alpha(0.65),
+                    CaretWidth = 3.2f,
+                    ExtensionLineColor = Color.Red.Alpha(0.6),
+                    ExtensionLineWidth = 2.8f
+                },
+                CursorPassive = new SheetMusicTheme.CursorTheme {
+                    CaretColor = Color.DarkSeaGreen.Alpha(0.5),
+                    CaretWidth = 2.6f,
+                    ExtensionLineColor = Color.IndianRed.Alpha(0.45),
+                    ExtensionLineWidth = 2f
+                },
+                TrackActive = new SheetMusicTheme.TrackTheme {
+                    Background = Color.Khaki,
+                    Foreground = Color.Black
+                },
+                TrackPassive = new SheetMusicTheme.TrackTheme {
+                    Background = Color.Beige,
+                    Foreground = Color.Black
+                }
+            },
+
+            SheetMusicPassive = new SheetMusicTheme {
+                CursorActive = new SheetMusicTheme.CursorTheme {
+                    CaretColor = Color.DarkSeaGreen.Alpha(0.5),
+                    CaretWidth = 2.6f,
+                    ExtensionLineColor = Color.IndianRed.Alpha(0.45),
+                    ExtensionLineWidth = 2f
+                },
+                CursorPassive = new SheetMusicTheme.CursorTheme {
+                    CaretColor = Color.DarkSeaGreen.Alpha(0.5),
+                    CaretWidth = 2.6f,
+                    ExtensionLineColor = Color.IndianRed.Alpha(0.45),
+                    ExtensionLineWidth = 2f
+                },
+                TrackActive = new SheetMusicTheme.TrackTheme {
+                    Background = Color.Beige,
+                    Foreground = Color.Black
+                },
+                TrackPassive = new SheetMusicTheme.TrackTheme {
+                    Background = Color.Beige,
+                    Foreground = Color.Black
+                }
+            }
+        };
+    }
+}
