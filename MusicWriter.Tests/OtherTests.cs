8b98e0f871124020392e8e020ebce51c1a11a51e
Isaac V <isaacv123456789@gmail.com>
2017-02-23 09:15:57 -0600

    added other tests

---
+++ MusicWriter.Tests/OtherTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MusicWriter.Tests
+{
+    [TestClass]
+    public class OtherTests
+    {
+        [TestMethod]
+        public void TestBitArray() {
+            var bitarray = new ShiftableBitArray();
+
+            for (int i = 0; i < 256; i++) {
+                var val = ((i / 32) % 2) == 1;
+
+                bitarray.Insert(i, val);
+
+                Assert.AreEqual(bitarray[i], val);
+            }
+            
+            for (int i = 0; i < 512; i++) {
+                var val =
+                    i < 256 ?
+                        ((i / 32) % 2) == 1 :
+                        false;
+
+                Assert.AreEqual(bitarray[i], val);
+            }
+        }
+    }
+}
