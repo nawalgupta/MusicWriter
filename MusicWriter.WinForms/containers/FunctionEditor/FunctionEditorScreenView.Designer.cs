e011e000202c3801ab3cdf91b52fbda636832f69
Isaac V <isaacv123456789@gmail.com>
2017-03-24 08:20:10 -0500

    reorganized folder structrure

---
+++ MusicWriter.WinForms/containers/FunctionEditor/FunctionEditorScreenView.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace MusicWriter.WinForms
+{
+    partial class FunctionEditorScreenView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ToolStripSeparator mnuPlaySeparator1;
+            System.Windows.Forms.Panel panel1;
+            System.Windows.Forms.Panel panel2;
+            this.mnuPlay = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuPlayTone = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPlayLength = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPlayTempo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPlayVerb = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPlayRepeat = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPlayPlay = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPlayStop = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tabFunctionSources = new System.Windows.Forms.TabControl();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.txtSearchFunctionSources = new System.Windows.Forms.TextBox();
+            this.btnDeleteFunctionSource = new System.Windows.Forms.Button();
+            this.btnAddFunctionSource = new System.Windows.Forms.Button();
+            this.txtPolylines = new System.Windows.Forms.TextBox();
+            this.btnDeletePolyline = new System.Windows.Forms.Button();
+            this.btnAddPolyline = new System.Windows.Forms.Button();
+            this.lsvFunctionSources = new System.Windows.Forms.ListView();
+            this.clmFunctionSourceName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lsvPolylines = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            mnuPlaySeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            panel1 = new System.Windows.Forms.Panel();
+            panel2 = new System.Windows.Forms.Panel();
+            this.mnuPlay.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuPlaySeparator1
+            // 
+            mnuPlaySeparator1.Name = "mnuPlaySeparator1";
+            mnuPlaySeparator1.Size = new System.Drawing.Size(108, 6);
+            // 
+            // mnuPlay
+            // 
+            this.mnuPlay.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuPlayTone,
+            this.mnuPlayLength,
+            this.mnuPlayTempo,
+            this.mnuPlayVerb,
+            this.mnuPlayRepeat,
+            mnuPlaySeparator1,
+            this.mnuPlayPlay,
+            this.mnuPlayStop});
+            this.mnuPlay.Name = "mnuPlay";
+            this.mnuPlay.Size = new System.Drawing.Size(112, 164);
+            // 
+            // mnuPlayTone
+            // 
+            this.mnuPlayTone.Name = "mnuPlayTone";
+            this.mnuPlayTone.Size = new System.Drawing.Size(111, 22);
+            this.mnuPlayTone.Text = "&Tone";
+            // 
+            // mnuPlayLength
+            // 
+            this.mnuPlayLength.Name = "mnuPlayLength";
+            this.mnuPlayLength.Size = new System.Drawing.Size(111, 22);
+            this.mnuPlayLength.Text = "&Length";
+            // 
+            // mnuPlayTempo
+            // 
+            this.mnuPlayTempo.Name = "mnuPlayTempo";
+            this.mnuPlayTempo.Size = new System.Drawing.Size(111, 22);
+            this.mnuPlayTempo.Text = "Te&mpo";
+            // 
+            // mnuPlayVerb
+            // 
+            this.mnuPlayVerb.Name = "mnuPlayVerb";
+            this.mnuPlayVerb.Size = new System.Drawing.Size(111, 22);
+            this.mnuPlayVerb.Text = "&Verb";
+            // 
+            // mnuPlayRepeat
+            // 
+            this.mnuPlayRepeat.Name = "mnuPlayRepeat";
+            this.mnuPlayRepeat.Size = new System.Drawing.Size(111, 22);
+            this.mnuPlayRepeat.Text = "&Repeat";
+            // 
+            // mnuPlayPlay
+            // 
+            this.mnuPlayPlay.Name = "mnuPlayPlay";
+            this.mnuPlayPlay.Size = new System.Drawing.Size(111, 22);
+            this.mnuPlayPlay.Text = "&Play";
+            // 
+            // mnuPlayStop
+            // 
+            this.mnuPlayStop.Name = "mnuPlayStop";
+            this.mnuPlayStop.Size = new System.Drawing.Size(111, 22);
+            this.mnuPlayStop.Text = "S&top";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabFunctionSources);
+            this.splitContainer1.Size = new System.Drawing.Size(600, 396);
+            this.splitContainer1.SplitterDistance = 200;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // tabFunctionSources
+            // 
+            this.tabFunctionSources.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabFunctionSources.Location = new System.Drawing.Point(0, 0);
+            this.tabFunctionSources.Name = "tabFunctionSources";
+            this.tabFunctionSources.SelectedIndex = 0;
+            this.tabFunctionSources.Size = new System.Drawing.Size(396, 396);
+            this.tabFunctionSources.TabIndex = 2;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.lsvFunctionSources);
+            this.splitContainer2.Panel1.Controls.Add(panel1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.lsvPolylines);
+            this.splitContainer2.Panel2.Controls.Add(panel2);
+            this.splitContainer2.Size = new System.Drawing.Size(200, 396);
+            this.splitContainer2.SplitterDistance = 186;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(this.txtSearchFunctionSources);
+            panel1.Controls.Add(this.btnDeleteFunctionSource);
+            panel1.Controls.Add(this.btnAddFunctionSource);
+            panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            panel1.Location = new System.Drawing.Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(200, 22);
+            panel1.TabIndex = 2;
+            // 
+            // txtSearchFunctionSources
+            // 
+            this.txtSearchFunctionSources.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSearchFunctionSources.Location = new System.Drawing.Point(0, 0);
+            this.txtSearchFunctionSources.Name = "txtSearchFunctionSources";
+            this.txtSearchFunctionSources.Size = new System.Drawing.Size(154, 20);
+            this.txtSearchFunctionSources.TabIndex = 3;
+            this.txtSearchFunctionSources.Text = "Function Sources";
+            // 
+            // btnDeleteFunctionSource
+            // 
+            this.btnDeleteFunctionSource.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnDeleteFunctionSource.Location = new System.Drawing.Point(154, 0);
+            this.btnDeleteFunctionSource.Name = "btnDeleteFunctionSource";
+            this.btnDeleteFunctionSource.Size = new System.Drawing.Size(23, 22);
+            this.btnDeleteFunctionSource.TabIndex = 2;
+            this.btnDeleteFunctionSource.Text = "-";
+            this.btnDeleteFunctionSource.UseVisualStyleBackColor = true;
+            // 
+            // btnAddFunctionSource
+            // 
+            this.btnAddFunctionSource.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnAddFunctionSource.Location = new System.Drawing.Point(177, 0);
+            this.btnAddFunctionSource.Name = "btnAddFunctionSource";
+            this.btnAddFunctionSource.Size = new System.Drawing.Size(23, 22);
+            this.btnAddFunctionSource.TabIndex = 1;
+            this.btnAddFunctionSource.Text = "+";
+            this.btnAddFunctionSource.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(this.txtPolylines);
+            panel2.Controls.Add(this.btnDeletePolyline);
+            panel2.Controls.Add(this.btnAddPolyline);
+            panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            panel2.Location = new System.Drawing.Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(200, 22);
+            panel2.TabIndex = 4;
+            // 
+            // txtPolylines
+            // 
+            this.txtPolylines.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPolylines.Location = new System.Drawing.Point(0, 0);
+            this.txtPolylines.Name = "txtPolylines";
+            this.txtPolylines.Size = new System.Drawing.Size(154, 20);
+            this.txtPolylines.TabIndex = 3;
+            this.txtPolylines.Text = "Polylines";
+            // 
+            // btnDeletePolyline
+            // 
+            this.btnDeletePolyline.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnDeletePolyline.Location = new System.Drawing.Point(154, 0);
+            this.btnDeletePolyline.Name = "btnDeletePolyline";
+            this.btnDeletePolyline.Size = new System.Drawing.Size(23, 22);
+            this.btnDeletePolyline.TabIndex = 2;
+            this.btnDeletePolyline.Text = "-";
+            this.btnDeletePolyline.UseVisualStyleBackColor = true;
+            // 
+            // btnAddPolyline
+            // 
+            this.btnAddPolyline.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnAddPolyline.Location = new System.Drawing.Point(177, 0);
+            this.btnAddPolyline.Name = "btnAddPolyline";
+            this.btnAddPolyline.Size = new System.Drawing.Size(23, 22);
+            this.btnAddPolyline.TabIndex = 1;
+            this.btnAddPolyline.Text = "+";
+            this.btnAddPolyline.UseVisualStyleBackColor = true;
+            // 
+            // lsvFunctionSources
+            // 
+            this.lsvFunctionSources.CheckBoxes = true;
+            this.lsvFunctionSources.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmFunctionSourceName});
+            this.lsvFunctionSources.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsvFunctionSources.Location = new System.Drawing.Point(0, 22);
+            this.lsvFunctionSources.Name = "lsvFunctionSources";
+            this.lsvFunctionSources.Size = new System.Drawing.Size(200, 164);
+            this.lsvFunctionSources.TabIndex = 3;
+            this.lsvFunctionSources.UseCompatibleStateImageBehavior = false;
+            this.lsvFunctionSources.View = System.Windows.Forms.View.Details;
+            // 
+            // clmFunctionSourceName
+            // 
+            this.clmFunctionSourceName.Text = "Name";
+            this.clmFunctionSourceName.Width = 192;
+            // 
+            // lsvPolylines
+            // 
+            this.lsvPolylines.CheckBoxes = true;
+            this.lsvPolylines.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lsvPolylines.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsvPolylines.Location = new System.Drawing.Point(0, 22);
+            this.lsvPolylines.Name = "lsvPolylines";
+            this.lsvPolylines.Size = new System.Drawing.Size(200, 184);
+            this.lsvPolylines.TabIndex = 5;
+            this.lsvPolylines.UseCompatibleStateImageBehavior = false;
+            this.lsvPolylines.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 192;
+            // 
+            // FunctionEditorScreenView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FunctionEditorScreenView";
+            this.Size = new System.Drawing.Size(600, 396);
+            this.Load += new System.EventHandler(this.FunctionEditorScreenView_Load);
+            this.mnuPlay.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip mnuPlay;
+        private System.Windows.Forms.ToolStripMenuItem mnuPlayTone;
+        private System.Windows.Forms.ToolStripMenuItem mnuPlayLength;
+        private System.Windows.Forms.ToolStripMenuItem mnuPlayTempo;
+        private System.Windows.Forms.ToolStripMenuItem mnuPlayVerb;
+        private System.Windows.Forms.ToolStripMenuItem mnuPlayRepeat;
+        private System.Windows.Forms.ToolStripMenuItem mnuPlayPlay;
+        private System.Windows.Forms.ToolStripMenuItem mnuPlayStop;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TabControl tabFunctionSources;
+        private System.Windows.Forms.TextBox txtSearchFunctionSources;
+        private System.Windows.Forms.Button btnDeleteFunctionSource;
+        private System.Windows.Forms.Button btnAddFunctionSource;
+        private System.Windows.Forms.TextBox txtPolylines;
+        private System.Windows.Forms.Button btnDeletePolyline;
+        private System.Windows.Forms.Button btnAddPolyline;
+        private System.Windows.Forms.ListView lsvFunctionSources;
+        private System.Windows.Forms.ColumnHeader clmFunctionSourceName;
+        private System.Windows.Forms.ListView lsvPolylines;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
---
+++ MusicWriter.WinForms/containers/FunctionEditor/FunctionEditorScreenView.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicWriter.WinForms
+{
+    public partial class FunctionEditorScreenView : UserControl
+    {
+        EditorFile file;
+        FunctionEditorScreen screen;
+
+        public EditorFile File {
+            get { return file; }
+            set {
+                if (file != null)
+                    throw new InvalidOperationException();
+
+                file = value;
+            }
+        }
+
+        public FunctionEditorScreen Screen {
+            get { return screen; }
+            set {
+                if (screen != null)
+                    throw new InvalidOperationException();
+
+                screen = value;
+            }
+        }
+
+        public FunctionEditorScreenView() {
+            InitializeComponent();
+        }
+
+        void Setup() {
+            ContextMenus.Attatch_Tone(mnuPlayTone, screen.DebugSound.Tone);
+
+            screen.FunctionSources.ItemInserted += FunctionSources_ItemInserted;
+            screen.FunctionSources.ItemWithdrawn -= FunctionSources_ItemWithdrawn;
+            screen.FunctionSources.ItemMoved -= FunctionSources_ItemMoved;
+        }
+
+        private void FunctionSources_ItemInserted(
+                FunctionSource item, 
+                int index
+            ) {
+            var control =
+                new FunctionSourceEditorControl();
+
+            control.Setup(item);
+
+            var tab = 
+                new TabPage();
+
+            tab.Controls.Add(control);
+            control.Dock = DockStyle.Fill;
+
+            ObservableProperty<string>.PropertyChangeHandler namechanged =
+                (old, @new) => tab.Text = @new;
+
+            item.Name.AfterChange += namechanged;
+            
+            tab.Disposed += delegate {
+                control.UnSetup();
+                item.Name.AfterChange -= namechanged;
+            };
+
+            tabFunctionSources.Controls.Add(tab);
+        }
+        
+        private void FunctionSources_ItemWithdrawn(
+                FunctionSource item,
+                int index
+            ) =>
+            tabFunctionSources
+                .Controls
+                .RemoveAt(index);
+
+        private void FunctionSources_ItemMoved(
+                FunctionSource item, 
+                int oldindex,
+                int newindex
+            ) =>
+            tabFunctionSources
+                .Controls
+                .SetChildIndex(
+                        tabFunctionSources.Controls[oldindex],
+                        newindex
+                    );
+        
+        private void FunctionEditorScreenView_Load(object sender, EventArgs e) {
+        }
+
+        private void tabFunctionSources_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void tabFunctionSources_ControlAdded(object sender, ControlEventArgs e) =>
+            tabFunctionSources.SelectedIndex = tabFunctionSources.Controls.Count - 1;
+    }
+}
