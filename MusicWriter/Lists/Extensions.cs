087d4d6fdb1887dda0b3706c94b9f12c72fdf563
Isaac V <isaacv123456789@gmail.com>
2017-04-25 09:21:34 -0500

    list extensions

---
+++ MusicWriter/Lists/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicWriter
+{
+    public static class ListExtensions
+    {
+        public static void Bind<T>(
+                this IObservableList<T> source,
+                IObservableList<T> sink
+            ) {
+            source.ItemAdded += sink.Add;
+            source.ItemRemoved += _ => sink.Remove(_);
+        }
+    }
+}
