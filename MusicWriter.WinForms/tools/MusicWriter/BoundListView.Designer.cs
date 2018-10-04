70ee1f1742f75a4ad91b58abd76386dc7bbae381
Isaac V <isaacv123456789@gmail.com>
2018-10-04 12:58:06 -0500

    interim commit

---
+++ MusicWriter.WinForms/tools/MusicWriter/BoundListView.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace MusicWriter.WinForms
+{
+    partial class BoundListView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tblLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.lsvItems = new System.Windows.Forms.ListView();
+            this.clmName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtSearch = new PlaceholderTextBox.PlaceholderTextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.tblLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tblLayout
+            // 
+            this.tblLayout.ColumnCount = 3;
+            this.tblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 22F));
+            this.tblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 22F));
+            this.tblLayout.Controls.Add(this.lsvItems, 0, 1);
+            this.tblLayout.Controls.Add(this.txtSearch, 0, 0);
+            this.tblLayout.Controls.Add(this.btnRemove, 1, 0);
+            this.tblLayout.Controls.Add(this.btnCreate, 2, 0);
+            this.tblLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblLayout.Location = new System.Drawing.Point(0, 0);
+            this.tblLayout.Name = "tblLayout";
+            this.tblLayout.RowCount = 2;
+            this.tblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblLayout.Size = new System.Drawing.Size(164, 242);
+            this.tblLayout.TabIndex = 0;
+            // 
+            // lsvItems
+            // 
+            this.lsvItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmName});
+            this.tblLayout.SetColumnSpan(this.lsvItems, 3);
+            this.lsvItems.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsvItems.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lsvItems.HideSelection = false;
+            this.lsvItems.Location = new System.Drawing.Point(0, 21);
+            this.lsvItems.Margin = new System.Windows.Forms.Padding(0);
+            this.lsvItems.Name = "lsvItems";
+            this.lsvItems.Size = new System.Drawing.Size(164, 221);
+            this.lsvItems.TabIndex = 1;
+            this.lsvItems.UseCompatibleStateImageBehavior = false;
+            this.lsvItems.View = System.Windows.Forms.View.Details;
+            this.lsvItems.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.lsvItems_ItemChecked);
+            this.lsvItems.SelectedIndexChanged += new System.EventHandler(this.lsvItems_SelectedIndexChanged);
+            // 
+            // clmName
+            // 
+            this.clmName.Text = "Name";
+            this.clmName.Width = 120;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(0, 0);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(0);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.PlaceholderText = "Search";
+            this.txtSearch.Size = new System.Drawing.Size(120, 20);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnRemove.Location = new System.Drawing.Point(120, 0);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(0);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(22, 21);
+            this.btnRemove.TabIndex = 0;
+            this.btnRemove.Text = "-";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCreate.Location = new System.Drawing.Point(142, 0);
+            this.btnCreate.Margin = new System.Windows.Forms.Padding(0);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(22, 21);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "+";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // BoundListView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tblLayout);
+            this.Name = "BoundListView";
+            this.Size = new System.Drawing.Size(164, 242);
+            this.tblLayout.ResumeLayout(false);
+            this.tblLayout.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tblLayout;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.ListView lsvItems;
+        private System.Windows.Forms.ColumnHeader clmName;
+        private PlaceholderTextBox.PlaceholderTextBox txtSearch;
+    }
+}
---
+++ MusicWriter.WinForms/tools/MusicWriter/BoundListView.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicWriter.WinForms
+{
+    public partial class BoundListView : UserControl
+    {
+        public event Action<StorageObjectID> ItemSelected;
+        public event Action<StorageObjectID> ItemChecked;
+        public event Action<StorageObjectID> ItemUnchecked;
+
+        public bool CheckBoxes {
+            get { return lsvItems.CheckBoxes; }
+            set { lsvItems.CheckBoxes = value; }
+        }
+
+        public BoundListView() {
+            InitializeComponent();
+        }
+
+        private void lsvItems_SelectedIndexChanged(object sender, EventArgs e) {
+            if (lsvItems.SelectedItems.Count > 0)
+                ItemSelected?.Invoke((StorageObjectID)lsvItems.SelectedItems[0].Tag);
+        }
+
+        private void lsvItems_ItemChecked(object sender, ItemCheckedEventArgs e) {
+            var objID = (StorageObjectID)e.Item.Tag;
+            if (e.Item.Checked)
+                ItemChecked?.Invoke(objID);
+            else
+                ItemUnchecked?.Invoke(objID);
+        }
+
+        private class ItemInfo
+        {
+            public ListViewItem ListViewItem;
+            public ObservableProperty<string>.PropertySetHandler NameSetResponder;
+        }
+
+        readonly Dictionary<StorageObjectID, ItemInfo> itemmap =
+            new Dictionary<StorageObjectID, ItemInfo>();
+
+        public void InsertItem<T>(T item, int i)
+            where T : INamedObject, IBoundObject<T> {
+            var info = new ItemInfo();
+            itemmap.Add(item.StorageObjectID, info);
+
+            info.ListViewItem = lsvItems.Items.Insert(i, "");
+            info.NameSetResponder = name => {
+                info.ListViewItem.Text = name;
+            };
+            item.Name.Set += info.NameSetResponder;
+            info.ListViewItem.Tag = item.StorageObjectID;
+        }
+        
+        public void MoveItem<T>(T item, int oldindex, int newindex)
+            where T : INamedObject, IBoundObject<T>  {
+            var lsvItem = itemmap[item.StorageObjectID].ListViewItem;
+            if (lsvItem.Index != oldindex)
+                throw new InvalidOperationException();
+
+            lsvItems.Items.Remove(lsvItem);
+            lsvItems.Items.Insert(newindex, lsvItem);
+        }
+
+        public void WithdrawItem<T>(T item, int i)
+            where T : INamedObject, IBoundObject<T>  {
+            if (itemmap[item.StorageObjectID].ListViewItem.Index != i)
+                throw new InvalidOperationException();
+            
+            itemmap[item.StorageObjectID].ListViewItem.Remove();
+            itemmap.Remove(item.StorageObjectID);
+        }
+
+        public abstract class Binder<T>
+            where T : INamedObject, IBoundObject<T>
+        {
+            readonly BoundListView listview;
+            readonly BoundList<T> boundlist;
+
+            public BoundListView ListView {
+                get { return listview; }
+            }
+
+            public BoundList<T> BoundList {
+                get { return boundlist; }
+            }
+
+            public Binder(
+                    BoundListView listview,
+                    BoundList<T> boundlist
+                ) {
+                this.listview = listview;
+                this.boundlist = boundlist;
+            }
+
+            public abstract void Bind();
+            public abstract void Unbind();
+        }
+
+        public sealed class ItemBinder<T>
+            : Binder<T>
+            where T : INamedObject, IBoundObject<T>
+        {
+            public ItemBinder(
+                    BoundListView listview,
+                    BoundList<T> boundlist
+                ) :
+                base(
+                        listview,
+                        boundlist
+                    ) {
+            }
+
+            public override void Bind() {
+                BoundList.ItemInserted += ListView.InsertItem;
+                BoundList.ItemWithdrawn += ListView.WithdrawItem;
+                BoundList.ItemMoved += ListView.MoveItem;
+            }
+
+            public override void Unbind() {
+                BoundList.ItemInserted -= ListView.InsertItem;
+                BoundList.ItemWithdrawn -= ListView.WithdrawItem;
+                BoundList.ItemMoved -= ListView.MoveItem;
+            }
+        }
+
+        public sealed class SelectorBinder<T>
+            : Binder<T>
+            where T : INamedObject, IBoundObject<T>
+        {
+            readonly Selector<T> selector;
+
+            public Selector<T> Selector {
+                get { return selector; }
+            }
+
+            public SelectorBinder(
+                    BoundListView listview,
+                    BoundList<T> boundlist,
+                    Selector<T> selector
+                ) :
+                base(
+                        listview,
+                        boundlist
+                    ) {
+                this.selector = selector;
+            }
+
+            public override void Bind() {
+                ListView.ItemSelected += ListView_ItemSelected;
+            }
+
+            public override void Unbind() {
+                ListView.ItemSelected -= ListView_ItemSelected;
+            }
+
+            private void ListView_ItemSelected(StorageObjectID objID) {
+                selector.Active.Value = BoundList[objID];
+            }
+        }
+    }
+}
